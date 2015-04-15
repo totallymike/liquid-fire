48f5e40f6766131b7130c62d50fe85efb5f89672
Michael Westbom <michael@agilion.com>
2015-04-15 16:14:28 -0400

    Demonstrate broken liquid-with behavior

--- tests/dummy/app/controllers/helpers-documentation/liquid-with/page.js
+++ tests/dummy/app/controllers/helpers-documentation/liquid-with/page.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
+  test: false,
   actions: {
     higher: function(model){
       this.transitionToRoute('helpers-documentation.liquid-with.page', model.get('id')+1);
